a253a5dcb90417ee196ce7dce14f30bda7b7e822
baumgarb <baumgarb@gmail.com>
2019-02-02 08:19:53 +0000

    display current date in specific format on app component and write e2e test which fakes the current date and checks whether the faked date is present or not (currently test is failing !)

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,16 +8,16 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to E2ETestWithJasmineClock!');
+  it('should display the fake date put in place via jasmine.clock().mockDate()', async () => {
+    jasmine.clock().install();
+    jasmine.clock().mockDate(new Date(2040, 0, 1));
+
+    await page.navigateTo();
+    const result = await page.getCurrentDateDisplayed();
+    expect(result).toEqual('2040-01-01');
+
+    jasmine.clock().uninstall();
   });
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    }));
-  });
+
 });
