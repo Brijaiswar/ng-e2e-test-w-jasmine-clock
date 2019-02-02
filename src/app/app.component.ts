570d924f85dbafb516db325b0b83a38cb0d1851c
baumgarb <baumgarb@gmail.com>
2019-02-02 08:40:48 +0000

    change html template to inline template rather than using a separate html template file. Remove unnecessary files in this case.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+  <div style="text-align:center">
+    <h1>
+      Current date is <span class="e2e-curr-date">{{currentDate | date:'yyyy-MM-dd'}}</span>.
+    </h1>
+  </div>`,
 })
 export class AppComponent {
-  title = 'E2ETestWithJasmineClock';
   currentDate = new Date();
 }
