c75fd57bd4c17d0e58e66c0165005e745e01466d
baumgarb <baumgarb@gmail.com>
2019-02-02 23:03:51 +0000

    moving installing and setup of jasmine.clock stuff into beforeAll, moving uninstall into afterAll

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -2,22 +2,21 @@ import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
-
-  it('should display the fake date put in place via jasmine.clock().mockDate()', async () => {
+  beforeAll(() => {
     jasmine.clock().install();
     jasmine.clock().mockDate(new Date(2040, 0, 1));
+  });
 
-    await page.navigateTo();
-    const result = await page.getCurrentDateDisplayed();
-    expect(result).toEqual('2040-01-01');
-
+  afterAll(() => {
     jasmine.clock().uninstall();
   });
 
+  it('should display the fake date put in place via jasmine.clock().mockDate()', async () => {
+    const page = new AppPage();
+    await page.navigateTo();
+    const result = await page.getCurrentDateDisplayed();
+    expect(result).toEqual('2040-01-01');
+  });
 
 });
